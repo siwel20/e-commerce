93683c7ab9bdac6b1a9c1f4cb6ecd7e25ea025de
siwel20 <plthrock@gmail.com>
2020-08-01 17:47:55 -0600

    added associations in models index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'//why is this the foreignKey id????
+});
 
 // Products belongToMany Tags (through ProductTag)
+Category.belongsToMany(Tag, {
+  through: ProductTag,
+  as: ['product_id', 'tag_id'], // why wouldn't we add "as:"" here????
+  foreignKey: 'tag_id' // is this the right foreignKey????
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'product_id' // is this the right foreign key??
+});
+
 
 module.exports = {
   Product,
