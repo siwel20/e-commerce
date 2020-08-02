c7bf0850db54db0399623faa86d0a7ee9fe4b18f
siwel20 <plthrock@gmail.com>
2020-08-02 14:21:20 -0600

    added POST, PUT and DELETE routes to category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -73,7 +73,7 @@ router.put('/:id', (req, res) => {
     }
   })
   .then(dbCategoryData => {
-    if (!dbCategoryData) {
+    if (!dbCategoryData[0]) {
       res.status(400).json({
         message: "No category found with this id"
       });
